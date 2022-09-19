dc1e35c28a59010fded2a397aca204dc4839f11d
FilipWil <fille_kbk@hotmail.com>
2022-09-19 17:18:22 +0200

    MainMenu
    Bild och musik till starting screen, men lite små andra ändringar

---
+++ Andreas Olsson/Assets/Scripts/MusicController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+
+    [SerializeField] private AudioClip musicClip;
+
+    void Start()
+    {
+        audioSource.clip = musicClip;
+        audioSource.loop = true;
+        audioSource.Play();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
