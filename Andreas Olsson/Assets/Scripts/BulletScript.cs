4a691fd02edb51a970f357296d1d4d1b6ab50df6
Charkorz <andres17@live.se>
2022-09-25 18:06:55 +0200

    Revert "Små ändring"
    This reverts commit 006cb076902e015a1a1db76a928b2fb6637a4fe2.

--- Andreas Olsson/Assets/Scripts/BulletScript.cs
+++ Andreas Olsson/Assets/Scripts/BulletScript.cs
@@ -13,11 +13,12 @@ public class BulletScript : MonoBehaviour
 
     void Start()
     {
+        
         bulletRB = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player");
         Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
         bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
-        Destroy(this.gameObject, 1);
+        Destroy(this.gameObject, 2);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
