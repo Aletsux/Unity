e0878b29ae631a3ba3436970bac7dcb4f6388086
Charkorz <andres17@live.se>
2022-09-25 21:03:27 +0200

    Update BulletScript

--- Andreas Olsson/Assets/Scripts/BulletScript.cs
+++ Andreas Olsson/Assets/Scripts/BulletScript.cs
@@ -18,7 +18,7 @@ public class BulletScript : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player");
         Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
         bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
-        Destroy(this.gameObject, 2);
+        Destroy(this.gameObject, 1);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
